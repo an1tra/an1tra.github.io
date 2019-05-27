6efc9cb005e2070e46aeaf43d80cb414717a95b4
Anitra <anitra.oursky@gmail.com>
2019-05-27 06:50:08 -0400

    Revisions

--- Giftastic/assets/js/app.js
+++ Giftastic/assets/js/app.js
@@ -2,36 +2,58 @@
 	 //"https://api.giphy.com/v1/gifs/search?api_key=dlBJEEg3yLQtOKfclJ2ukWFYJtJFRgi6&q=" + search + "&limit=10&offset=0&rating=g&lang=en&fmt=JSON"
 	 
 	
-	 // Initial array of searches
+	 // Initial Array of searches
       var searches = ["Plane", "Boat", "Spaceship", "Tesla"];
 
-      // displaySearchInfo function re-renders the HTML to display the appropriate content
+      // FUNCTION#1 - Dynamically creates buttons for items found in Initial Array.
+      function renderButtons() {
+
+        // Deleting the search buttons prior to adding new search button
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#buttons-view").empty();
+
+        // Looping through the Initial Array
+        for (var i = 0; i < searches.length; i++) {
+
+          // Dynamicaly generates buttons for each search in Initial Array
+          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adding a class of search-btn to each button
+          a.addClass("search-btn");
+          // Adding a data-attribute to each button
+          a.attr("data-name", searches[i]);
+          // Providing the initial button text
+          a.text(searches[i]);
+          // Adding the button to the buttons-view div
+          $("#buttons-view").append(a);
+        }
+      }
+
+      // FUNCTION #2 - Attaches API to each rendered button.
       function displaySearchInfo() {
 
         var gifSearch = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ gifSearch +"&api_key=TGbi0yc7vp9Kg5pCAaIiHcnsgEtYnSs0&limit=10&offset=0&rating=g&lang=en";
 
         // Creating an AJAX call for the specific search button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-         
+        $.ajax({url: queryURL, method: "GET"}).then(function(response) {
+          
           var results = response.data;
+          console.log(results);
           for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div>");
-            gifDiv.addClass("flex");
+            var gifDiv = $("<div>"); //Creates div to store gifs
+            gifDiv.addClass("flex");//Gives flex positioning to elements in div
 
-            var gifImages = $("<img>");
-            gifImages.attr("class", "animate");
-            //Basically building the element in the gif pause class activity.
+            var gifImages = $("<img>");//Creates image tags
+            gifImages.attr("class", "animate"); //Gives each newly created image a class of animate.
+            //Basically building the image element in the gif pause class activity.
             // When the gifs populate you want them to appear "still" so you set the image tag source to the still url.
-            gifImages.attr("src", results[i].images.fixed_height_still.url);
-            gifImages.attr("data-still", results[i].images.fixed_height_still.url);
-            gifImages.attr("data-animate", results[i].images.fixed_height.url);
+            gifImages.attr("src", results[i].images.original_still.url);
+            gifImages.attr("data-still", results[i].images.original_still.url);
+            gifImages.attr("data-animate", results[i].images.original.url);
             gifImages.attr("data-state", "still");
             
-            
+            //Dumps all newly created image tags in the div designated to store gifs
             gifDiv.append(gifImages);
             $("#searches-view").prepend(gifDiv);
 
@@ -41,10 +63,11 @@
 
       }
 
-
+     //Function #3 - Animating the still gifs
       $(document).on("click", ".animate", function() {
+        //this - refering to the element with the class "animate"
         var state = $(this).attr("data-state");
-    
+     //Intentionally set the data state to still so that when clicked it will change the source to the animated url AND change the data-state
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
@@ -55,45 +78,19 @@
     
     });
 
-      // Function for displaying search data
-      function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-
-        // Looping through the array of movies
-        for (var i = 0; i < searches.length; i++) {
-
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of search-btn to our button
-          a.addClass("search-btn");
-          // Adding a data-attribute
-          a.attr("data-name", searches[i]);
-          // Providing the initial button text
-          a.text(searches[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
-
-      // This function handles events where a search button is clicked
+      //FUNCTION #4: Makes the Add Search Button Work
       $("#add-search").on("click", function(event) {
         event.preventDefault();
-        // This line grabs the input from the textbox
+        //Grabs the input from the textbox
         var search = $("#search-input").val().trim();
 
-        // Adding search from the textbox to our array
+        // Puts the input into the Initial Array.
         searches.push(search);
 
         // Calling renderButtons which handles the processing of our searches array
         renderButtons();
       });
-
-      
-
 
       // Adding a click event listener to all elements with a class of "search-btn"
       $(document).on("click", ".search-btn", displaySearchInfo);
