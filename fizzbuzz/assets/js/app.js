be21461ed162ee40054687f549a817ca87f045da
Anitra <anitra.oursky@gmail.com>
2019-12-05 09:59:32 -0500

    added comments to explain code

--- fizzbuzz/assets/js/app.js
+++ fizzbuzz/assets/js/app.js
@@ -4,13 +4,13 @@ let emptyArray = [];
 function fizzbuzz() {
     
    
-    for(let i = 0; i<=100; i++) {
+    for(let i = 0; i<=100; i++) { //Here we create a loop that runs from 0 to 100.
 
-        if(i % 3 === 0 && i % 5 === 0) {
-            emptyArray.push("fizzbuzz");
+        if(i % 3 === 0 && i % 5 === 0) { //Here we get the modulous of each number, pass it through conditions that check if it is divisible by both 3 & 5. 
+            emptyArray.push("fizzbuzz"); //If this condition is true we push "fizzbuzz" into the empty array.
         }
     
-        else if(i % 3 === 0) {
+        else if(i % 3 === 0) { //Must seperate i modulo 3 from i modulo 5 into else if statements due to different desired outputs as opposed to  using the or(||) operator.
             emptyArray.push("fizz");
         } 
         
@@ -18,7 +18,7 @@ function fizzbuzz() {
             emptyArray.push("buzz");
         }
         else {
-            emptyArray.push(i);
+            emptyArray.push(i);//If none of the previously defined conditions are true just push i into the array. 
         }
     } 
     
